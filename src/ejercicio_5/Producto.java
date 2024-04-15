67cdb69feecdf7ee6ab6852126a211cf4b54ecfa
diegostampa <163307116+diegostampa@users.noreply.github.com>
2024-04-15 15:44:31 -0300

    Initial commit

---
+++ src/ejercicio_5/Producto.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_5;
+
+/**
+ *
+ * @author stamp
+ */
+public class Producto {
+    private int codigo;
+    private String descripcion;
+    private double precio;
+    private String categoria;
+
+    public Producto() {
+    }
+
+    public Producto(int codigo, String descripcion, double precio, String categoria) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+    
+}
